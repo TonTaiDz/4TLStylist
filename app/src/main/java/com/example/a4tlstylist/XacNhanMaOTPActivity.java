261987a9bfd550df1a3f1155c3ddad04fdb2af18
Vinh <nviet7532@gmail.com>
2023-11-30 14:10:47 +0700

    create layout

---
+++ app/src/main/java/com/example/a4tlstylist/XacNhanMaOTPActivity.java
@@ -0,0 +1,14 @@
+package com.example.a4tlstylist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class XacNhanMaOTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xac_nhan_ma_otpactivity);
+    }
+}
